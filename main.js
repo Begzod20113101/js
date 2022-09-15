45b6e77b2422d3c3a36c6faf4f3c59c6755438b8
Begzod20113101 <akbaevanilufar@gmail.com>
2022-09-15 19:29:32 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,123 @@
+// var maxStudent = 5;
+// var jssStudents = [];
+// var smmStudents = [];
+
+
+// function addStudents(Student){
+//     if (Student.includes("JS")){
+//         jssStudents.push(Student)
+//     }
+
+//     if (jssStudents.length == maxStudent) {
+//         console.log("You can not Join this group");
+//     }
+
+// }
+// var hello = "hello";
+// var hello = "gud morning";
+
+
+
+
+// let sum = 0;
+// const arr = [1,2,3,4,5];
+
+// for (let i =0; i < arr.length; i++){
+//     sum += arr[i];
+// }
+
+
+
+// const phone = "iphone14pro";
+
+
+// switch (phone) {
+//     case "anilmal":
+//         console.log("not buy");
+//     break
+//     case "iphone14pro":
+        
+//         console.log("You can buy this phone");
+//         break
+//     case  "samson":
+        
+//         console.log("there is not more new phone");
+//         break   
+// }
+
+
+// const monday = "";
+
+
+// switch(monday) {
+//     case "monday":
+//         console.log("true");
+//         break
+//         case "sandey":
+//         console.log("false");
+// break
+// case "wednesday":
+//     console.log("false");
+// break
+// case "sandey":
+//     console.log("false");
+// break
+// case "sandey":
+//     console.log("false");
+// break
+// case "sandey":
+//     console.log("false");
+// break
+// case "sandey":
+//     console.log("false");
+// break
+// }
+
+
+
+
+// const age = 6;
+
+// const canSwim = age > 18 ? "swim" : age > 20 ? "Can not swim" : "you cannot swim"
+
+
+// let obj = {
+//     name:"Begzod",
+//     sorname: "Safarov",
+//     age: 11 
+
+// }
+// console.log(obj.name);
+
+
+
+// function main(a){
+//    let namArr = 0
+//    let strArr = ""
+
+//    if(typeof a === "string"){
+//     strArr.push(a)
+//     return strArr
+//    }
+// }
+
+// console.log(main(1));
+
+
+
+
+
+
+// logname(5)
+
+// function logname(n,k){
+//     console.log(k*n);
+// }
+
+
+
+
+
+
+
+
